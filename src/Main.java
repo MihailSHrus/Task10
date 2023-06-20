353d2e40582906d3eb117047255ce190dea38d9a
MihailSHrus <97229510+MihailSHrus@users.noreply.github.com>
2023-06-20 17:01:14 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args){
+        int k = 1;
+        while (k < 11) {
+            System.out.println("Task"+k);
+            k++;
+        }
+    }
+}
